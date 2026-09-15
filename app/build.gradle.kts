867414e97abdcfa3b29d2706fa090497baab78c6
Nima Hakim <nimahakim.ad@gmail.com>
2026-09-15 13:45:15 +0330

    prepare for release 1.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.nima.app.imanage"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
